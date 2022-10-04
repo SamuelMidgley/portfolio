296fde6d4beedbe75f49858970bb9dc67e9a033a
Samuel Midgley <midgley.sam@gmail.com>
2022-10-04 17:58:16 +0100

    update readme

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="content">
-      <p className="subtitle">Hi, I'm Sam, I am shit at coding</p>
+      <p className="subtitle">Hi, I'm Sam, I am coding</p>
       <a href="https://github.com/SamuelMidgley">
         <h3 className="title">@SamMidgley</h3>
       </a>
