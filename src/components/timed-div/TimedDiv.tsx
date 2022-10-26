09b3558bee2cdb324fb7e68e334dea479644efc7
Samuel Midgley <midgley.sam@gmail.com>
2022-10-26 22:01:57 +0100

    remove console.logs

--- src/components/timed-div/TimedDiv.tsx
+++ src/components/timed-div/TimedDiv.tsx
@@ -8,13 +8,10 @@ interface TimeDivProps {
 }
 
 const TimedDiv = (props: TimeDivProps) => {
-  console.log('hello')
   const { text, state, setState } = props
-  console.log(state)
 
   useEffect(() => {
     setTimeout(function () {
-      console.log('finished')
       setState(false)
     }, 10000)
   }, [state, setState])
