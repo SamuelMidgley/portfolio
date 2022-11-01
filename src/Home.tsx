165a482f0854bd2d4183bdcb8e5184ee9893aa61
Samuel Midgley <midgley.sam@gmail.com>
2022-11-01 22:50:59 +0000

    funky

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
 import './Home.css'
 
 export default function Home() {
+  const [isFunky, setIsFunky] = useState(false)
+
+  const onClickHandler = () => {
+    setIsFunky((prevFunky) => !prevFunky)
+  }
+
   return (
     <div className="cool_thing">
-      <div className="content">
+      <div
+        className={`content ${isFunky ? 'funky' : ''}`}
+        onClick={onClickHandler}
+      >
         <p className="subtitle">Hi, I'm Sam, I can follow a tutorial</p>
         <a href="https://github.com/SamuelMidgley">
           <h3 className="title">@SamMidgley</h3>
