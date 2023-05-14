f35bf3f52f502cd5ae1621275b93d2d45b440953
Samuel Midgley <midgley.sam@gmail.com>
2023-05-14 01:11:42 +0100

    Adding "About me" section 🧑‍💻

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,11 +1,13 @@
-import { Header } from './components'
+import { About, Header, NavBar } from './components'
 import './Home.scss'
 
 export default function Home() {
   return (
     <div className="home">
+      <NavBar />
       <main className="main">
         <Header />
+        <About />
       </main>
     </div>
   )
